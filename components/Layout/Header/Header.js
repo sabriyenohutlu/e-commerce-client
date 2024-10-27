55f975dd27d4f6dbb5f3c92c0d16190c1a4661fb
sabriyeuregen <sabriyeuregen@gmail.com>
2024-10-27 22:12:16 +0300

    Added : icons

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -1,23 +1,44 @@
+import Link from "next/link";
 import "./header.scss";
+import { FaTshirt } from "react-icons/fa";
+import { TiUser } from "react-icons/ti";
+import { GrFavorite } from "react-icons/gr";
+import { GrBasket } from "react-icons/gr";
 const Header = () => {
   return (
     <div className="header">
       <div className="header-top">
-        <span>LOGO</span>
+        <Link className="header-top__logo" href={"/"}>
+          <FaTshirt size={35} color="#E6C744" />
+        </Link>
         <div className="header-top__search">
           <input
             type="text"
-            className="header-top__search"
-            placeholder="What are you looking for?"
+            className="header-top__search-input"
+            placeholder="Aradığınız ürünü yazın..."
           />
           <button type="submit" className="header-top__search-button">
             <i className="header-top__search-button-icon"></i>
           </button>
         </div>
         <div className="header-top__customerInfo">
-        <span>Hesabım</span>
-        <span>Favorilerim</span>
-        <span>Sepetim</span>
+          <Link className="header-top__customerInfo-profile" href={"/hesabim"}>
+            <TiUser />
+            <span>Hesabım</span>
+          </Link>
+
+          <Link
+            href={"/favorilerim"}
+            className="header-top__customerInfo-favorite"
+          >
+            <GrFavorite />
+            <span>Favorilerim</span>
+          </Link>
+
+          <Link href={"/sepet"} className="header-top__customerInfo-basket">
+            <GrBasket />
+            <span>Sepetim</span>
+          </Link>
         </div>
       </div>
       <div className="header-bottom">
--- components/MainSlider/MainSlider.js
+++ components/MainSlider/MainSlider.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import "./mainSlider.scss";
+import anaSayfaTisort from "../../public/images/ana-sayfa-tisort.png";
+
 const MainSlider = () => {
   return (
-    <div className="mainSlider">MainSlider</div>
+    <div className="mainSlider">
+      <Image src={anaSayfaTisort} className="mainSlider-image"/>
+    </div>
   )
 }
 
