7c0e2703a00a9848a089087b6c1669b9123d34f9
sabriyenohutlu <sabriyeuregen@gmail.com>
2025-01-05 18:39:27 +0300

    Added : fake api

---
+++ app/products/page.js
@@ -0,0 +1,25 @@
+"use client";
+import ProductCard from "@/components/CategoryComponents/ProductsListComponents/ProductCard/ProductCard";
+import { fetchProducts } from "@/data/data";
+import { useEffect, useState } from "react";
+
+const ProductDenemePage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      async function loadProducts() {
+        const data = await fetchProducts();
+        setProducts(data);
+      }
+  
+      loadProducts();
+    }, []);
+    console.log(products)
+  return (
+    <div>
+        {products.map((product,idx) => <ProductCard key={idx} product={product}/>)}
+    </div>
+  )
+}
+
+export default ProductDenemePage
---
+++ data/data.js
@@ -0,0 +1,15 @@
+export async function fetchProducts() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const products = await response.json();
+        return products;
+      
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+fetchProducts();
